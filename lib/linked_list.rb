5568695f95c9dc105620df1fd58848667ad1e7a4
Adam DePaolo <amdepaolo@gmail.com>
2023-04-04 22:50:43 -0400

    commit tree 02bd81021ffedaa3047c588b7e6fb0f8159b173e

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,25 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    if head == tail
+      return
+    end
+
+    curr_node = tail
+
+    while curr_node
+      next_on_deck = curr_node.prev_node
+      new_next = curr_node.prev_node
+      new_prev = curr_node.next_node
+      curr_node.next_node = new_next
+      curr_node.prev_node = new_prev
+      curr_node = next_on_deck
+    end
+
+    new_head = self.tail
+    new_tail = self.head
+    self.head = new_head
+    self.tail = new_tail
+
   end
 end
